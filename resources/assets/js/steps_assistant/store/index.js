9c92941771b7d4265a4162bd42001c1155db3f1f
ponichKing <ponichproof@gmail.com>
2018-12-10 13:22:04 +0300

    Update index.js

--- resources/assets/js/steps_assistant/store/index.js
+++ resources/assets/js/steps_assistant/store/index.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import settingsModule from './settings'
 import moduleWidget from './widget'
-import moduleLog from './log'
-import statsModule from './stats'
 
 Vue.use(Vuex)
 
@@ -11,8 +9,6 @@ const store = new Vuex.Store({
   modules: {
     settings: settingsModule,
     widget: moduleWidget,
-    log: moduleLog,
-    stats: statsModule
   }
 })
 
